b58ec4b4d040c17c134c6050590578a019def4c5
devinrbopp <devinrbopp@gmail.com>
2021-12-10 07:09:12 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
+import Poster from './Poster'
+import Display from './Display'
+import Form from './Form';
+
 function App() {
+
+  const [bounties, setBounties] = useState([])
+  const [current, setCurrent] = useState({})
+  
+  const handleClick = (bounty) => {
+    setCurrent(bounty)
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:8000/bounties')
+    .then(response => {
+      return response.json()
+    })
+    .then(foundBounties => {
+      console.log(foundBounties)
+      setBounties(foundBounties)
+    })
+  }, [])
+  
+  const posters = bounties.map(b => {
+    return <Poster bounty={b} key={b.name} handleClick={handleClick}/>
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Wanted</h1>
+        <Display bounty={current} />
       </header>
+      <section className="Poster-Board">
+        {posters}
+      </section>
+      <section className="App-header">
+        <Form />
+      </section>
     </div>
   );
 }
